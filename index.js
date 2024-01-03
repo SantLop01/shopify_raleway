163ed29901988f5ffc92574c8801b8e2cc57b4b7
Santiago López <gabsant2001@gmail.com>
2024-01-03 02:29:36 -0300

    Cambio a usar body parser y agregado de rutas no encontradas

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express();
 const path = require('path')
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const { notFoundPage } = require('./src/utils/errorHandlers');
 
 // Traer los manejadores de rutas
 const mainRoutes = require('./src/routes/mainRoutes');
@@ -32,7 +34,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, "./src/views"));
 
 // Parsea los datos recibidos por POST
-app.use(express.urlencoded());
+app.use(bodyParser.urlencoded());
 app.use(express.json());
 
 // Method override para habilitar PUT y DELETE
@@ -43,6 +45,8 @@ app.use('/', mainRoutes);
 app.use('/admin', adminRoutes);
 app.use('/shop', shopRoutes);
 app.use('/auth', authRoutes);
+
+app.use(notFoundPage);
 
 // Server up
 app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}/home`))
