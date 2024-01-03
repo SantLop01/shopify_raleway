a8f683a3d7e22477c9b83cf96b1838855e51ec71
Santiago López <gabsant2001@gmail.com>
2024-01-03 01:26:02 -0300

    deleted conflictive dependenci

--- src/middlewares/uploadFiles.js
+++ src/middlewares/uploadFiles.js
@@ -1,5 +1,4 @@
 const multer = require('multer');
-const sharp = require('sharp');
 const path = require('path');
 
 const storage = multer.diskStorage({
@@ -8,20 +7,6 @@ const storage = multer.diskStorage({
 });
 
 const uploadFiles = multer({storage});
-
-// const pathImg = `${__dirname}../../../public/img`;
-
-// const uploadFiles = multer({
-//     storage: multer.memoryStorage(),
-//     onFileUploadStart: function(file) {
-//         sharp(file.path)
-//           .webp()
-//           .toFile(pathImg)
-//           .then(() => {
-//             console.log('Convertido a webp')
-//           })
-//     }
-// });
 
 module.exports = {
     uploadFiles
